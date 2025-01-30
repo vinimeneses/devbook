0d3f4f2af267fa40c3810a57e33360841ca5a431
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-01-30 15:43:12 -0300

    create sql table and publicacao struct

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
+DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
     id int auto_increment primary key,
     nome varchar(50) not null,
     nick varchar(50) not null unique,
     email varchar(50) not null unique,
-    senha varchar(255) not null,
+    senha varchar(50) not null,
     criadoEm timestamp default current_timestamp()
 ) ENGINE=INNODB;
 
@@ -25,4 +26,16 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
 
     primary key(usuario_id, seguidor_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null
+    autor_id int not null,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
 ) ENGINE=INNODB;
