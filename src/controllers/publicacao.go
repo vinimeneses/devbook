1e183b1ca1d253d41fb95ba0b0a5167018d10dfb
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-02-04 15:39:44 -0300

    method get 'publicacao'

--- src/controllers/publicacao.go
+++ src/controllers/publicacao.go
@@ -6,9 +6,13 @@ import (
 	"api/src/modelos"
 	"api/src/repositorios"
 	"api/src/respostas"
+	"database/sql"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"io"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
@@ -31,6 +35,11 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 	publicacao.AutorID = usuarioID
 
+	if erro = publicacao.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
 	db, erro := banco.Conectar()
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
@@ -47,17 +56,44 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
-//func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
-//
-//}
-//
-//func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
-//}
-//
-//func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
-//
-//}
-//
-//func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
-//
-//}
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+	defer func(db *sql.DB) {
+		erro := db.Close()
+		if erro != nil {
+			log.Fatal(erro)
+		}
+	}(db)
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
+}
+
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -40,3 +40,36 @@ func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, err
 
 	return uint64(ultimoIDInserido), nil
 }
+
+func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
+	linha, erro := repositorio.db.Query(`
+		select p.*, u.nick from
+		publicacoes p inner join usuarios u
+		on u.id = p.autor_id where p.id = ?
+		`, publicacaoID)
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+	defer func(linha *sql.Rows) {
+		err := linha.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(linha)
+
+	var publicacao modelos.Publicacao
+	if linha.Next() {
+		if erro := linha.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+	return publicacao, nil
+}
