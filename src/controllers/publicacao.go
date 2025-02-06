aaef69dce28a07c530fcc11ae3fe4f7b3d07d988
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-02-06 16:49:14 -0300

    update and delete crud methods

--- src/controllers/publicacao.go
+++ src/controllers/publicacao.go
@@ -8,6 +8,7 @@ import (
 	"api/src/respostas"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"github.com/gorilla/mux"
 	"io"
 	"log"
@@ -114,9 +115,108 @@ func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
 }
 
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer func(db *sql.DB) {
+		erro := db.Close()
+		if erro != nil {
+			log.Fatal(erro)
+		}
+	}(db)
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicacaoSalvaNoBanco.AutorID != usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("você não pode alterar uma publicação que não é sua"))
+		return
+	}
+
+	corpoDaRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoDaRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = publicacao.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+	}
+
+	if erro = repositorio.Atualizar(publicacaoID, publicacao); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
 }
 
 func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer func(db *sql.DB) {
+		erro := db.Close()
+		if erro != nil {
+			log.Fatal(erro)
+		}
+	}(db)
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicacaoSalvaNoBanco.AutorID != usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("você não pode deletar uma publicação que não é sua"))
+		return
+	}
+
+	if erro = repositorio.Deletar(publicacaoID); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
 }
--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -81,7 +81,8 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, e
 		inner join usuarios u on u.id = p.autor_id
 		inner join seguidores s on p.autor_id = s.usuario_id
 	where u.id = ? or s.seguidor_id = ?
-	`, usuarioID, usuarioID)
+	order by 1 desc`,
+		usuarioID, usuarioID)
 	if erro != nil {
 		return nil, erro
 	}
@@ -111,4 +112,43 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, e
 		publicacoes = append(publicacoes, publicacao)
 	}
 	return publicacoes, nil
+}
+
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo = ?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer func(statement *sql.Stmt) {
+		erro := statement.Close()
+		if erro != nil {
+			log.Fatal(erro)
+		}
+	}(statement)
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from publicoes where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer func(statement *sql.Stmt) {
+		erro := statement.Close()
+		if erro != nil {
+			log.Fatal(erro)
+		}
+	}(statement)
+
+	_, erro = statement.Exec(publicacaoID)
+	if erro != nil {
+		return erro
+	}
+
+	return nil
 }
